884e48f158eefb1a68ee1e5d8f2734cf895b6e41
Vaidehi0207 <vaidehi.priya02@gmail.com>
2024-07-09 16:24:13 +0530

    website done

---
+++ edusity/src/App.jsx
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+const App = () => {
+
+  const [playState, setPlaystate] = useState(false);
+  //update this value when we click on playicon
+
+
+
+  return ( 
+    <div>
+        <Navbar/>
+        <Hero/>
+        <div className='container'>
+            {/* mount the title file */}
+            <Title subTitle='Our PROGRAM' title='What We Offer'/>
+        <Programs/>
+        <About setPlaystate={setPlaystate}/>
+        <Title subTitle='Gallary' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get In Touch'/>
+        <Contact/>
+        <Footer/>
+        </div>
+        <VideoPlayer playState={playState} setPlaystate={setPlaystate}/>
+    </div>
+  )
+}
+
+export default App
